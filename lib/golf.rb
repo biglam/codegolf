850d47756f023dd5d5c9da0c2f91fdb67d14d253
Craig Lamont <craig@techiteasy.org.uk>
2015-12-03 11:38:24 +0000

    made shorter 2

--- lib/golf.rb
+++ lib/golf.rb
@@ -3,16 +3,12 @@ class Golf
 
   def s.hole1(v)
     (1..v).map do |x|
-      ppp = {3=>'pling',5 =>'plang',7=>'plong'}.map {|f,n| n if x % f == 0}
-      ppp.any? ? ppp.join : x
+      p = {3=>'pling',5 =>'plang',7=>'plong'}.map {|f,n| n if x % f == 0}
+      p.any? ? p.join : x
     end
   end
 
   def s.hole2(w)
-    s = ""
-    w.split(' ').map { |l|
-      s += l[0]
-    }
-    s
+    w.split(' ').map {|l| l[0]}.join
   end
 end
