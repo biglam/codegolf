3000cf8892d22c6502c3739051222d06d4251681
Craig Lamont <craig@techiteasy.org.uk>
2015-12-03 11:30:25 +0000

    all tests passing

--- lib/golf.rb
+++ lib/golf.rb
@@ -1,3 +1,30 @@
 class Golf
+  s = self
   
+  def s.ppp(v)
+    ppp = {3=>'pling',5 =>'plang',7=>'plong'}.map {|f,n| n if v % f == 0}
+    ppp.any? ? ppp.join : v
+  end
+
+  def s.hole1(v)
+    a=[]
+    (1..v).map do |x|
+      a << ppp(x)
+    end
+    a
+  end
+
+  def s.hole2(w)
+    s = ""
+    w.split(' ').map { |l|
+      s += l[0]
+    }
+    s
+  end
+
 end
+
+
+
+
+
