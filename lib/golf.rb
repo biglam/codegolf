5088bfde7298052ec38adc7f7ef5fc2eb1a6e306
Craig Lamont <craig@techiteasy.org.uk>
2015-12-03 11:55:24 +0000

    less

--- lib/golf.rb
+++ lib/golf.rb
@@ -1,14 +1,14 @@
 class Golf
   s = self
 
-  def s.hole1(v)
+  def s.hole1 v
     (1..v).map { |x|
       p = {3=>'pling',5=>'plang',7=>'plong'}.map {|f,n| n if x % f == 0}
       p.any? ? p.join : x
     }
   end
 
-  def s.hole2(w)
+  def s.hole2 w
     w.split(' ').map {|l| l[0]}.join
   end
 end
